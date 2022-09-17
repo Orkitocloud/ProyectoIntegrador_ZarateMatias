55d83ccc2e5689bbf7b3ecaaaad91f8454d82f60
Cloud <88631476+Orkitocloud@users.noreply.github.com>
2022-09-17 11:36:03 -0300

    Ultima Modificacion
    Ultima Modificacion

--- ProyectoIntegrador_ZarateMatias/src/app/app.module.ts
+++ ProyectoIntegrador_ZarateMatias/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogosComponent } from './components/logos/logos.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogosComponent,
+    SocialComponent,
+    BannerComponent,
+    AboutComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
